5ac064fcd7335fc9b71e97b4265f2fcc3b719f1c
AtesiT <atesit@mail.ru>
2026-03-10 14:32:04 +0300

    ADD: library TimeLine from github and init the UINib in TimelineView

--- FlightTimeline/TimelineView.swift
+++ FlightTimeline/TimelineView.swift
@@ -4,11 +4,16 @@ struct TimelineView: UIViewControllerRepresentable {
     
     //  После того, как создаём метод - убираем слово some и устанавливаем ViewController, который хотим получить. После этого вызываем метод updateUIViewController
     func makeUIViewController(context: Context) -> UITableViewController {
-        
+        UITableViewController()
     }
     
     func updateUIViewController(_ uiViewController: UITableViewController, context: Context) {
+        let timelineTableViewCell = UINib(nibName: "TimelineTableViewCell", bundle: nil)
         
+        uiViewController.tableView.register(
+            timelineTableViewCell,
+            forCellReuseIdentifier: "TimelineTableViewCell"
+        )
     }
     
 }

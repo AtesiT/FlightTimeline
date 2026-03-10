47497e0c60878e7b6855071d51a0cc0395005c66
AtesiT <atesit@mail.ru>
2026-03-10 15:10:15 +0300

    ADD: extension TimelineView(with final class Coordinator)

--- FlightTimeline/TimelineView.swift
+++ FlightTimeline/TimelineView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 struct TimelineView: UIViewControllerRepresentable {
+    let flights: [String]
     
     //  После того, как создаём метод - убираем слово some и устанавливаем ViewController, который хотим получить. После этого вызываем метод updateUIViewController
     func makeUIViewController(context: Context) -> UITableViewController {
@@ -14,10 +15,38 @@ struct TimelineView: UIViewControllerRepresentable {
             timelineTableViewCell,
             forCellReuseIdentifier: "TimelineTableViewCell"
         )
+        
+        uiViewController.tableView.dataSource = context.coordinator
     }
     
+    func makeCoordinator() -> Coordinator {
+        Coordinator(flights: flights)
+    }
+}
+
+extension TimelineView {
+    final class Coordinator: NSObject, UITableViewDataSource {
+        var flights: [String]
+        
+        init(flights: [String]) {
+            self.flights = flights
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            flights.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let flight = flights[indexPath.row]
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TimelineTableViewCell", for: indexPath) as! TimelineTableViewCell
+            cell.descriptionLabel.text = flight
+            return cell
+        }
+        
+        
+    }
 }
 
 #Preview {
-    TimelineView()
+    TimelineView(flights: ["London - Berlin", "Los Angeles - New York"])
 }

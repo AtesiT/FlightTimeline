e9f221eae2aa34d5fd9fdd6e94fe6081e345ab5e
AtesiT <atesit@mail.ru>
2026-03-10 14:16:42 +0300

    INIT: Two methods in TimelineView (UIViewControllerRepresentable)

---
+++ FlightTimeline/TimelineView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct TimelineView: UIViewControllerRepresentable {
+    
+    //  После того, как создаём метод - убираем слово some и устанавливаем ViewController, который хотим получить. После этого вызываем метод updateUIViewController
+    func makeUIViewController(context: Context) -> UITableViewController {
+        
+    }
+    
+    func updateUIViewController(_ uiViewController: UITableViewController, context: Context) {
+        
+    }
+    
+}
+
+#Preview {
+    TimelineView()
+}

77b4044b4c2eb17e977cc2d8d85bd0123f1c58ca
AtesiT <atesit@mail.ru>
2026-03-19 12:07:40 +0300

    MADE: init arrivals and departures (that contain flights but filtered), links exist pass

--- FlightTimeline/HomeView.swift
+++ FlightTimeline/HomeView.swift
@@ -4,27 +4,43 @@ struct HomeView: View {
     //  Во flights будет находится сгенерированный массив
     private let flights = FlightInformation.generateFlights()
     
+    private var arrivals: [FlightInformation] {
+        flights.filter { $0.direction == .arrival }
+    }
+    
+    private var departures: [FlightInformation] {
+        flights.filter { $0.direction == .departure }
+    }
+    
     var body: some View {
-        ZStack {
-            Image(systemName: "airplane")
-                .resizable()
-                .frame(width: 250, height: 250)
-                .opacity(0.1)
-                .rotationEffect(.degrees(-90))
-            
-            VStack {
-                NavigationLink("Arrivals") {
-                    FlightBoardView(title: "Arrivals", fligths: flights)
-                }
+        //  NavigationStack позволяет сделать кликабельными ссылки (из серого цвета станут синими)
+        NavigationStack {
+            ZStack {
+                Image(systemName: "airplane")
+                    .resizable()
+                    .frame(width: 250, height: 250)
+                    .opacity(0.1)
+                    .rotationEffect(.degrees(-90))
                 
-                NavigationLink("Departures") {
-                    FlightBoardView(title: "Departures", fligths: flights)
+                VStack(alignment: .leading, spacing: 10) {
+                    NavigationLink("Arrivals") {
+                        FlightBoardView(title: "Arrivals", fligths: arrivals)
+                    }
+                    
+                    NavigationLink("Departures") {
+                        FlightBoardView(title: "Departures", fligths: departures)
+                    }
+                    
+                    NavigationLink("Flight Timeline") {
+                        TimelineView(flights: flights)
+                    }
+                    
+                    Spacer()
                 }
-
-                NavigationLink("Flight Timeline") {
-                    TimelineView(flights: flights)
-                }
+                .font(.title)
+                .padding()
             }
+            .navigationTitle("Airport")
         }
     }
 }

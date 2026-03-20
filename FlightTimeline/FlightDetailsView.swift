3316376f797b50bdac092dd1a3cfc2997193be60
AtesiT <atesit@mail.ru>
2026-03-20 13:09:35 +0300

    ADD: FlightDetailsView (that contains Info about a flight)

---
+++ FlightTimeline/FlightDetailsView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct FlightDetailsView: View {
+    let flight: FlightInformation
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            //  Добавляем HStack и Spacer ниже, чтобы сместить влево текст
+            HStack {
+                Text("\(flight.airline) - Flight \(flight.number)")
+                    .font(.title)
+                Spacer()
+            }
+            
+            Text("\(flight.direction == .arrival ? "From" : "To") \(flight.otherAirport)")
+            
+            Text("\(flight.flightStatus)")
+                .foregroundStyle(Color(flight.timelineColor))
+            
+            Spacer()
+            
+            
+        }
+        .padding()
+        .font(.headline)
+    }
+}
+
+#Preview {
+    FlightDetailsView(flight: FlightInformation.generateFlight())
+}
